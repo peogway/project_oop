6f104142d4ed67e34d8bfdc2acf46a0e2728444b
peogway <duchungbeo2004@example.com>
2024-04-25 04:22:14 +0300

    first commit

---
+++ app/src/main/java/com/example/prj_oop/EmploymentRateData.java
@@ -0,0 +1,27 @@
+package com.example.prj_oop;
+
+public class EmploymentRateData {
+    private int year;
+    private float employmentRate;
+
+    public  EmploymentRateData(int year, float employmentRate){
+        this.year = year;
+        this.employmentRate = employmentRate;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public float getEmploymentRate() {
+        return employmentRate;
+    }
+
+    public void setEmploymentRate(float employmentRate) {
+        this.employmentRate = employmentRate;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
